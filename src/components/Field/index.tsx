9d27e3c052c21e542a77cbd492fe548c906c8819
eliotks <Ntnu1580>
2020-11-18 11:21:17 +0100

    Laget gameweeks.json. Tanken er at vi manuelt legger inn data her - denne filen er ganske enkel å endre og enkel hente ut data fra

--- src/components/Field/index.tsx
+++ src/components/Field/index.tsx
@@ -3,19 +3,29 @@ import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import PersonIcon from '@material-ui/icons/Person';
 import { ReactComponent as FieldBackground } from '../../field.svg';
+import gameweeks from '../../data/gameweeks.json';
 import './index.css';
 
 function Field() {
+  
+  // Kan vi sende props (hvilken gameweek det er snakk om) fra GameWeekList?
+
+  // Kan hente ut "color" fra gameweeks.json og gi til 'PersonIcon'
+
+  const gameweek = 9;
+
   return (
     <div className="root">
         <div>
-            <FieldBackground/>
+            <FieldBackground />
         </div>
        <div className="team">
             <Grid container direction="column" className="rootGrid">
                 <Grid item container direction="row" justify="center">
                     <Grid item >
                         <PersonIcon style={{ fontSize: 100 }}/>
+                        <Typography align="center" style={{ fontSize: 20 }}>{gameweeks[gameweek]["keeper"]["name"]}</Typography>
+                        <Typography align="center" style={{ fontSize: 20 }}>p: {gameweeks[gameweek]["keeper"]["prediction"]} , s: {gameweeks[gameweek]["keeper"]["score"]}</Typography>
                     </Grid>
                 </Grid>
                 <Grid item container direction="row" justify="space-between" className="defGrid">
--- src/components/GameWeekList/index.tsx
+++ src/components/GameWeekList/index.tsx
@@ -18,7 +18,7 @@ type IProps = WithStyles<typeof styles>
 const GameWeekList: React.FunctionComponent<IProps> = props =>  {
     const { classes } = props;
 
-    const gameweeks: string[] = ["8", "7", "6", "5", "4", "3", "2", "1"]
+    const gameweeks: string[] = ["9", "8", "7", "6", "5", "4"]
 
     return (
         <div className={classes.root}>
