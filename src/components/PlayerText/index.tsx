8fe9e79fecfaa6e39bad48914cc57736144378ec
eliotks <Ntnu1580>
2020-11-23 14:47:06 +0100

    Nå vises navn og score for hvver spiller tydeligere

--- src/components/PlayerText/index.tsx
+++ src/components/PlayerText/index.tsx
@@ -5,33 +5,19 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import { IPlayer } from '../../data/interfaces';
+import './index.css';
 
 
-const styles = {
-  root: {
-    paddingLeft: 2,
-    paddingRight: 2,
-  }
-}
-
-
-type IProps = WithStyles<typeof styles> & IPlayer
+type IProps = IPlayer
 
 const PlayerText = (props: IProps) => {
-  const { classes, name, team, prediction, score } = props
+  const { name, team, prediction, score } = props
   return (
-    <Card className={classes.root}>
-      <Typography align="center" style={{ fontSize: 8, fontWeight: "bold" }}>{name}</Typography>
-      <Grid container justify="space-around">
-        <Grid>
-        <Typography align="center" style={{ fontSize: 8, fontWeight: "bold" }}>{"Pred: " + prediction.toString()}</Typography>
-        </Grid>
-        <Grid>
-        <Typography align="center" style={{ fontSize: 8, fontWeight: "bold" }}>{"Score: " + score.toString()}</Typography>
-        </Grid>
-      </Grid>
-    </Card>
+    <div className="playerText">
+      <div className="playerName">{name}</div>
+      <div className="playerScore">{score}</div>     
+    </div>
   );
 }
 
-export default withStyles(styles)(PlayerText);
+export default PlayerText;
