ac3ff7b3a758596d8ec873fb2cec3cdce18e69f7
markusmk <markus.malum.kim@gmail.com>
2021-01-01 16:13:44 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -13,36 +13,21 @@ import {
   Route,
 } from "react-router-dom";
 
-// import Image1 from './pictures/alex-KEEpi-SOM_s-unsplash.jpg';
-// import Image2 from './pictures/henrique-macedo-8RzMsxk3wgg-unsplash.jpg';
-// import Image3 from './pictures/vienna-reyes-qCrKTET_09o-unsplash.jpg';
-import Image4 from './pictures/kpio.jpg';
-// import Image5 from './pictures/willian-justen-de-vasconcellos-KqpDuf1i83w-unsplash.jpg';
-// import Image6 from './pictures/marble.jpg';
-// import Image7 from './pictures/marble1.jpg';
-// import Image8 from './pictures/marble2.png';
-// import Image9 from './pictures/marble3.png';
-// import Image10 from './pictures/marble4.jpg';
-// import Image11 from './pictures/taudalpoi1.jpg';
-// import Image12 from './pictures/taudalpoi2.jpg';
-// import Image13 from './pictures/taudalpoi3.jpg';
-// import Image14 from './pictures/plain1.jpg';
+import Image from './pictures/kpio.jpg';
+
 
 const styles = {
   root: {
-    backgroundImage:  `url(${Image4})`,
-    //background: "#34507d",
+    backgroundImage:  `url(${Image})`,
     minHeight: '100vh',
     backgroundSize: "100%",
     backgroundRepeat: "repeat",
   },
-  semiRoot: {
-    //backdropFilter: "blur(3px)",
-  }
 };
 
 
 type IProps = WithStyles<typeof styles>
+
 
 const App: React.FunctionComponent<IProps> = props => {
   const { classes } = props;
@@ -51,7 +36,6 @@ const App: React.FunctionComponent<IProps> = props => {
       <ScrollToTop/>
       <Header/>
       <div className={classes.root}>
-        <div className={classes.semiRoot}>
           {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
           <Switch>
@@ -64,7 +48,6 @@ const App: React.FunctionComponent<IProps> = props => {
           </Switch>
           <Footer/>
         </div>
-      </div>
     </Router>
   );
 }
